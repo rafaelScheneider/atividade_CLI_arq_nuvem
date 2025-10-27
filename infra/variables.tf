ea70f7d44e4f732fc96008220835ab1ea28462d9
rafael_scheneider <rafael.ranaudo@sptech.school>
2025-10-27 12:04:18 -0300

    fix(): nome do bucket

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,7 +2,7 @@ variable "aws_region" {
  default = "us-east-1"
 }
 variable "bucket_name" {
-  default = "auto-bucket-healt-data-monitor"
+  default = "auto-bucket-health-data-monitor"
 }
 
 resource "random_id" "suffix" {
